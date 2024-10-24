ef5cc13511a3755e505f4bdbdb24d1ba3e98a143
DylanSilva-R <dylansilvarw@gmail.com>
2024-10-23 21:56:18 -0400

    The only thing left for bankers algorithm is calculating the availabel matrix and also the printing function.

--- Project_Two/Phase_Two/CPUDeadlock.c
+++ Project_Two/Phase_Two/CPUDeadlock.c
@@ -1,10 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include "Merge.h"
+
+#define MAX_METRIC 10
+#define MIN_METRIC 5
+#define MAX_PROCESSES 10
+#define MIN_PROCESSES 5
+#define MAX_RESOURCES 10
+#define MIN_RESOURCES 5
+
+/*
+* TODO:
+* 1) Implement deadlock conditions 
+*   1.1) Mutual exclusion
+*   1.2) Hold and wait
+*   1.3) No preemption
+*   1.4) Circular wait
+* 2) Implement deadlock detection: Banker's algorithm
+* 3) Simulate
+* 4) Deadlock Handling: Banker's algorithm
+*
+*/
+
+struct Process_With_R // Struct to define processes with resources
+{
+    char processID [13];
+    int resourceAllocation[4];
+    int resourceRequestMax[4];
+    int resourceAvailable[4];
+    int resourceNeed[4];
+};
+
+void * process_Action() // Process performing an action. Process threads will use this function.
+{
+
+}
+
+void print_Bankers_Algo(struct Process_With_R * pArray, int sizeOfPArray, int resourceCount)
+{
+    
+}
+
+void bankers_Algo_Avoidance(struct Process_With_R * pArray, int sizeOfPArray, int resourceCount)
+{
+    // First, calculate total for all process allocations.
+
+    int deadlockDetected = 0;
+    char safeSequence[resourceCount];
+    int resourceAllocationTotals[resourceCount];
+    int total;
+    int calcNeed;
+    
+    for(int i = 0; i < resourceCount; i++)
+    {
+        total = 0;
+
+        for(int j = 0; j < sizeOfPArray; j++)
+        {
+            total += pArray[j].resourceAllocation[i];
+        }
+
+        resourceAllocationTotals[i] = total;
+    }
+
+
+    // printf("Totals");
+    // for(int i = 0; i < resourceCount; i++)
+    // {
+    //     printf("R%d %d ", i, resourceAllocationTotals[i]);
+    // }
+    // printf("\n");
+
+    // Second, calculate need matrix
+
+    for(int i = 0; i < sizeOfPArray; i++)
+    {
+        for(int j = 0; j < resourceCount; j++)
+        {
+            calcNeed = pArray[i].resourceRequestMax[j] - pArray[i].resourceAllocation[j];
+            pArray[i].resourceNeed[j] = calcNeed;
+        }
+    }
+
+    printf("Need matrix\n");
+    for(int i = 0; i < sizeOfPArray; i++)
+    {
+        for(int j = 0; j < resourceCount; j++)
+        {
+            printf("R%d %d ", j, pArray[i].resourceNeed[j]);
+        }
+        printf("\n");
+    }
+
+    // Third, calculate available matrix
+
+
+}
+
+void test_Bankers_Algo()
+{
+    struct Process_With_R pArray[] = {{"P0", {0, 0, 1, 2}, {0, 0, 1, 2}, {1, 5, 2, 0}, {0, 0, 0, 0}}, // Process, allocation, max resource request, resource need
+                                     {"P1", {1, 0, 0, 0}, {1, 7, 5, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+                                     {"P2", {1, 3, 5, 4}, {2, 3, 5, 6}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+                                     {"P3", {0, 6, 3, 2}, {0, 6, 5, 2}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+                                     {"P4", {0, 0, 1, 4}, {0, 6, 5, 6}, {0, 0, 0, 0}, {0, 0, 0, 0}}};
+    int sizeOfPArray = sizeof(pArray) / sizeof(pArray[0]); // Size of process array.
+    int resourceCount = sizeof(pArray[1].resourceAllocation) /  sizeof(int); // Size of resources.
+
+    bankers_Algo_Avoidance(pArray, sizeOfPArray, resourceCount);
+}
 
 
 int main()
 {
-    printf("Hello.\n");
+    test_Bankers_Algo();
 
     return 1;
 }
---
+++ Project_Two/Phase_Two/Merge.h
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Process
+{
+    char ProcessID[13];
+    int arrivalTime;
+    int burstTime;
+    int waitTime;
+    int turnaroundTime;
+    // int resourceAllocation;
+    // int resourceRequest;
+};
+
+void merge(struct Process * pArray, int left, int mid, int right, int type) // Divide
+{
+    int i, j, k;
+    int newLeftHalf = mid - left + 1;
+    int newRightHalf = right - mid;
+
+    // Allocate memory for left and right arrays
+    struct Process * leftPArray = (struct Process *)malloc(newLeftHalf * sizeof(struct Process));
+    struct Process * rightPArray = (struct Process *)malloc(newRightHalf * sizeof(struct Process));
+
+    //struct Process leftPArray [newLeftHalf];
+    //struct Process rightPArray [newRightHalf];
+
+    // Copy data to left and right arrays.
+    for(i = 0; i < newLeftHalf; i++)
+    {
+        leftPArray[i] = pArray[left + i];
+    }
+
+    for(j = 0; j < newRightHalf; j++)
+    {
+        rightPArray[j] = pArray[mid + 1 + j];
+    }
+
+
+    // Merge left and right arrays beack into
+    i = 0;
+    j = 0;
+    k = left;
+
+    //  Merge process
+
+    if(type == 0)
+    {
+        while(i < newLeftHalf && j < newRightHalf)
+        {
+            // Sort relative to arrival time.
+            if (leftPArray[i].arrivalTime <= rightPArray[j].arrivalTime) 
+            {
+                pArray[k] = leftPArray[i];
+                i++;
+            }
+            else 
+            {
+                pArray[k] = rightPArray[j];
+                j++;
+            }
+            k++;        
+        }
+    }else
+    {
+        while(i < newLeftHalf && j < newRightHalf)
+        {
+            // Sort relative to burst time.
+            if (leftPArray[i].burstTime <= rightPArray[j].burstTime) 
+            {
+                pArray[k] = leftPArray[i];
+                i++;
+            }
+            else 
+            {
+                pArray[k] = rightPArray[j];
+                j++;
+            }
+            k++;        
+        }
+    }
+
+
+    // Put remaining values if left array and right array.
+
+    while(i < newLeftHalf)
+    {
+        pArray[k] = leftPArray[i];
+        i++;
+        k++;
+    }
+
+    while(j < newRightHalf)
+    {
+        pArray[k] = rightPArray[j];
+        j++;
+        k++;
+    }
+
+    free(leftPArray);
+    free(rightPArray);
+}
+
+void mergeSort(struct Process * pArray, int left, int right, int type) // Conquer and combine
+{
+    // int type will tell the merge function which attributes to sort by.
+
+    if(left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(pArray, left, mid, type); // Sort left half.
+        mergeSort(pArray, mid + 1, right, type); // Sort right half.
+
+        merge(pArray, left, mid, right, type);
+    }
+}
